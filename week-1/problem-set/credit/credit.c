2cc6ea3da69ebf0854b60c8cf13a3383fdaa5ae0
Daniel <Daniel.Burbridge.Developer@gmail.com>
2025-12-10 12:09:28 +0800

    Major Refactor, add validity checking

--- week-1/problem-set/credit/credit.c
+++ week-1/problem-set/credit/credit.c
@@ -1,118 +1,175 @@
 #include <cs50.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_PREFIXES 5
 #define MAX_LENGTHS 2
 #define CARD_VARIATIONS 3
 
+typedef struct {
+  string prefixes[MAX_PREFIXES];
+  int num_prefixes;
+  int valid_lengths[MAX_LENGTHS];
+  int num_lengths;
+  string display_name;
+} CardInformation;
+
+typedef struct {
+  string name;
+  CardInformation info;
+} CreditCard;
+
+typedef struct {
+  string number_as_string;
+  int digit_count;
+} PotentialCard;
+
+PotentialCard request_user_input_as_card(char string_buffer[]) {
+  long user_input = get_long("Number: ");
+  int chars_written = sprintf(string_buffer, "%ld", user_input);
+  string user_input_as_string = string_buffer;
+
+  PotentialCard users_potential_card = {
+      .number_as_string = user_input_as_string, .digit_count = chars_written};
+
+  return users_potential_card;
+}
+
+bool validate_card(PotentialCard card) {
+  string card_number = card.number_as_string;
+  int digit_count = card.digit_count;
+
+  int sum = 0;
+  int temp_num = 0;
+  for (int i = digit_count - 2; i >= 0; i -= 2) {
+    temp_num = ((card_number[i] - '0') * 2);
+    if (temp_num < 10) {
+      sum += temp_num;
+    } else {
+      int temp_num_one = temp_num % 10;
+      int temp_num_two = (temp_num / 10) % 10;
+      int temp_num_three = temp_num_one + temp_num_two;
+      sum += temp_num_three;
+    }
+  }
+
+  for (int i = digit_count - 1; i >= 0; i -= 2) {
+    sum += (card_number[i] - '0');
+  }
+
+  return sum % 10 == 0;
+}
+
+void get_card_type_from_length(PotentialCard card, CreditCard *CreditCardStore,
+                               string potential_card_types[],
+                               int *potential_card_types_count) {
+  int digit_count = card.digit_count;
+
+  for (int i = 0; i < CARD_VARIATIONS; i++) {
+    for (int j = 0; j < CreditCardStore[i].info.num_lengths; j++) {
+      if (digit_count == CreditCardStore[i].info.valid_lengths[j]) {
+        potential_card_types[*potential_card_types_count] =
+            CreditCardStore[i].info.display_name;
+        (*potential_card_types_count)++;
+      }
+    }
+  }
+
+  for (int i = 0; i < *potential_card_types_count; i++) {
+    // printf("Potential Card Type by length: %s\n", potential_card_types[i]);
+  }
+}
+
+void get_card_type_from_prefix(PotentialCard card, CreditCard *CreditCardStore,
+                               string potential_card_types[],
+                               int *potential_card_types_count) {
+  int digit_count = card.digit_count;
+  string card_number = card.number_as_string;
+
+  char prefix_buffer[30];
+
+  for (int l = 0; l < digit_count; l++) {
+    for (int m = 0; m <= l; m++) {
+      prefix_buffer[m] = card_number[m];
+      prefix_buffer[m + 1] = '\0';
+    }
+    string current_prefix = prefix_buffer;
+    // printf("Current Prefix: %s\n", current_prefix);
+    for (int j = 0; j < CARD_VARIATIONS; j++) {
+      // printf("Current Card: %s\n", CreditCardStore[j].info.display_name);
+      for (int k = 0; k < CreditCardStore[j].info.num_prefixes; k++) {
+        string card_prefix = CreditCardStore[j].info.prefixes[k];
+        // printf("Comapring Current Prefix: %s with Card Prefix %s\n",
+        //        current_prefix, card_prefix);
+        if (strcmp(current_prefix, card_prefix) == 0) {
+          // printf("MATCH\n");
+          potential_card_types[*potential_card_types_count] =
+              CreditCardStore[j].info.display_name;
+          (*potential_card_types_count)++;
+        }
+      }
+    }
+  }
+
+  for (int i = 0; i < *potential_card_types_count; i++) {
+    // printf("Potential Card Type by Prefix: %s\n", potential_card_types[i]);
+  }
+}
+
 int main(void) {
-  // This seems terrible ineffecient to allocate large arrays, and then use less
-  // of their space. But that's okay, one day I'll know how to avoid doing it
-  // this way.
-
-  typedef struct {
-    int prefixes[MAX_PREFIXES];
-    int num_prefixes;
-    int valid_lengths[MAX_LENGTHS];
-    int num_lengths;
-    string display_name;
-  } CardInformation;
-
-  typedef struct {
-    string name;
-    CardInformation info;
-  } CreditCard;
-
   CreditCard CreditCardStore[CARD_VARIATIONS] = {
       {"American Express",
-       {.prefixes = {34, 37},
+       {.prefixes = {"34", "37"},
         .num_prefixes = 2,
         .valid_lengths = {15},
         .num_lengths = 1,
         .display_name = "AMEX"}},
       {"MasterCard",
-       {.prefixes = {51, 52, 53, 54, 55},
+       {.prefixes = {"51", "52", "53", "54", "55"},
         .num_prefixes = 5,
         .valid_lengths = {16},
         .num_lengths = 1,
         .display_name = "MASTERCARD"}},
       {"Visa",
-       {.prefixes = {4},
+       {.prefixes = {"4"},
         .num_prefixes = 1,
         .valid_lengths = {13, 16},
         .num_lengths = 2,
         .display_name = "VISA"}},
   };
 
-  // There is an error in my logic here that I cannot solve. Numbers such as
-  // "01" (leading with a 0) digits are incorrectly assessed, as storing them as
-  // a long immediately truncates the leading 0. As none of the credit-cards can
-  // be pre-fixed with a 0, I'm choosing to ignore this error for now instead of
-  // changing implementation to ask the user for a string and deal with
-  // type-casting.
+  char string_buffer[30];
+  PotentialCard card_to_check = request_user_input_as_card(string_buffer);
+  // printf("card_to_check_number: %s\n", card_to_check.number_as_string);
+  bool valid = validate_card(card_to_check);
 
-  printf("USER INPUT:\n");
-  long user_input = get_long("Number: ");
-  int cc_length = 0;
-  if (user_input == 0) {
-    cc_length = 1;
+  if (valid != true) {
+    printf("INVALID");
   } else {
-    long temp = user_input;
-    while (temp > 0) {
-      temp /= 10;
-      cc_length++;
-    }
-  }
+    string potential_card_types_length[CARD_VARIATIONS];
+    int potential_card_types_length_count = 0;
+    get_card_type_from_length(card_to_check, CreditCardStore,
+                              potential_card_types_length,
+                              &potential_card_types_length_count);
 
-  int cc_prefix = user_input / ((cc_length));
+    string potential_card_types_prefix[CARD_VARIATIONS];
+    int potential_card_types_prefix_count = 0;
+    get_card_type_from_prefix(card_to_check, CreditCardStore,
+                              potential_card_types_prefix,
+                              &potential_card_types_prefix_count);
 
-  // string potential_cards_based_on_prefix[CARD_VARIATIONS];
-  // int potential_cards_based_on_prefix_count = 0;
-  string potential_cards_based_on_length[CARD_VARIATIONS];
-  int potential_cards_based_on_length_count = 0;
-
-  printf("\nCARD CHECKS BEGIN:\n");
-  // Determine if a Card matches the length of a known Card.
-  for (int i = 0; i < CARD_VARIATIONS; i++) {
-    printf("Checking Card: %s\n", CreditCardStore[i].name);
-    for (int j = 0; j < CreditCardStore[i].info.num_lengths; j++) {
-      printf("Checking CC length of %d against valid length of %d\n", cc_length,
-             CreditCardStore[i].info.valid_lengths[j]);
-      if (cc_length == CreditCardStore[i].info.valid_lengths[j]) {
-        printf("Match found!\n");
-        potential_cards_based_on_length[potential_cards_based_on_length_count] =
-            CreditCardStore[i].info.display_name;
-        potential_cards_based_on_length_count++;
+    string card_type = "INVALID";
+    for (int i = 0; i < potential_card_types_length_count; i++) {
+      for (int k = 0; k < potential_card_types_prefix_count; k++) {
+        if (strcmp(potential_card_types_length[i],
+                   potential_card_types_prefix[k]) == 0) {
+          card_type = potential_card_types_length[i];
+          break;
+        }
       }
     }
-  }
 
-  // // Determine if a Card matches the prefix of a known Card.
-  // for (int i = 0; i < CARD_VARIATIONS; i++) {
-  //   for (int j = 0; j < CreditCardStore[i].info.num_prefixes; j++) {
-  //     if (cc_length == CreditCardStore[i].info.prefixes[j]) {
-  //       potential_cards_based_on_prefix[potential_cards_based_on_prefix_count]
-  //       =
-  //           CreditCardStore[i].info.display_name;
-  //     }
-  //   }
-  // }
-
-  printf("\nDEBUG LOG:\n");
-  printf("This is your number: %li\n", user_input);
-  printf("Just so I'm using the variable and make stops yelling at me - %s\n",
-         CreditCardStore[1].info.display_name);
-
-  printf("your input was %d digit(s) long\n", cc_length);
-
-  printf("Your numbers starts with the digits %d\n\n", cc_prefix);
-
-  printf("You have %d potential matches based on length\n",
-         potential_cards_based_on_length_count);
-
-  for (int i = 0; i < potential_cards_based_on_length_count; i++) {
-    printf("Based off the length of your card, it could be a: %s\n",
-           potential_cards_based_on_length[i]);
+    printf("%s", card_type);
   }
 }
