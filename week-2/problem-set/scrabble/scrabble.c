6e2e3bd5249631339286b048fd7ee20b62433e06
Daniel <Daniel.Burbridge.Developer@gmail.com>
2025-12-10 17:23:15 +0800

    cringe if statement... if it works it works.

--- week-2/problem-set/scrabble/scrabble.c
+++ week-2/problem-set/scrabble/scrabble.c
@@ -6,6 +6,26 @@ string get_input_from_player(int player) {
   return get_string("Player %d: ", player + 1);
 }
 
+int silly_letter_to_point_conversation(char c) {
+  if (c == 'a' || c == 'e' || c == 'i' || c == 'l' || c == 'n' || c == 'o' ||
+      c == 'r' || c == 's' || c == 't' || c == 'u') {
+    return 1;
+  } else if (c == 'd' || c == 'g') {
+    return 2;
+  } else if (c == 'b' || c == 'c' || c == 'm' || c == 'p') {
+    return 3;
+  } else if (c == 'f' || c == 'h' || c == 'v' || c == 'w' || c == 'y') {
+    return 4;
+  } else if (c == 'k') {
+    return 5;
+  } else if (c == 'j' || c == 'x') {
+    return 8;
+  } else if (c == 'q' || c == 'z') {
+    return 10;
+  }
+  return 0;  // Should not happen for valid letters
+}
+
 int score_answer(string answer) {
   char string_buffer[50];
   int answer_length = sprintf(string_buffer, "%s", answer);
@@ -15,7 +35,7 @@ int score_answer(string answer) {
   int sum = 0;
   for (int i = 0; i < answer_length; i++) {
     answer[i] = tolower(answer[i]);
-    int points = answer[i] - 'a' + 1;
+    int points = silly_letter_to_point_conversation(answer[i]);
     printf("%c is worth %d points, adding to %d equals, %d\n", answer[i],
            points, sum, points + sum);
     sum += points;
@@ -24,6 +44,9 @@ int score_answer(string answer) {
   printf("SCORE: %d\n", sum);
   return sum;
 }
+
+// I'm not sure on Dict / Hashmap syntax for C. Does it even exist?
+// I don't even know switch statement. Forgive me for this code...
 
 int main(void) {
   const int PLAYERS = 2;
