1d55a2686e4eb7b94c7c0a8d2624c01a53808b23
Daniel <Daniel.Burbridge.Developer@gmail.com>
2025-12-13 19:27:12 +0800

    resolve majority calculation error

--- week-3/problem-set/runoff/runoff.c
+++ week-3/problem-set/runoff/runoff.c
@@ -137,7 +137,7 @@ void tabulate(void) {
 // Print the winner of the election, if there is one
 bool print_winner(void) {
   for (int i = 0; i < candidate_count; i++) {
-    if (candidates[i].votes > candidate_count / 2) {
+    if (candidates[i].votes > (voter_count / 2)) {
       printf("%s", candidates[i].name);
       return true;
     }
