155c3df1861a73c1d4cee5a8faea69319cea0a68
Daniel <Daniel.Burbridge.Developer@gmail.com>
2025-12-21 19:13:08 +0800

    FEAT: prototype reflect

--- week-4/problem-set/filter-less/helpers.c
+++ week-4/problem-set/filter-less/helpers.c
@@ -54,7 +54,18 @@ void sepia(int height, int width, RGBTRIPLE image[height][width]) {
 }
 
 // Reflect image horizontally
-void reflect(int height, int width, RGBTRIPLE image[height][width]) { return; }
+void reflect(int height, int width, RGBTRIPLE image[height][width]) {
+  RGBTRIPLE reversed[height][width];
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      RGBTRIPLE pixel = image[height][width];
+      reversed[i][width - (j + 1)] = pixel;
+    }
+  }
+
+  image = reversed;
+  return;
+}
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width]) { return; }
