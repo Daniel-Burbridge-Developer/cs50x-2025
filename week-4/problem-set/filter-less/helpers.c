77479f1df1b6b560c3e70cc57e23c467931f4d16
Daniel <Daniel.Burbridge.Developer@gmail.com>
2025-12-21 18:49:25 +0800

    FEAT: Grayscale

--- week-4/problem-set/filter-less/helpers.c
+++ week-4/problem-set/filter-less/helpers.c
@@ -1,25 +1,31 @@
 #include "helpers.h"
 
+#include <math.h>
+#include <stdio.h>
+
 // Convert image to grayscale
-void grayscale(int height, int width, RGBTRIPLE image[height][width])
-{
-    return;
+void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      BYTE red = image[i][j].rgbtRed;
+      BYTE green = image[i][j].rgbtGreen;
+      BYTE blue = image[i][j].rgbtBlue;
+
+      int average = round((red + blue + green) / 3.0);
+
+      image[i][j].rgbtRed = average;
+      image[i][j].rgbtGreen = average;
+      image[i][j].rgbtBlue = average;
+    }
+  }
+  return;
 }
 
 // Convert image to sepia
-void sepia(int height, int width, RGBTRIPLE image[height][width])
-{
-    return;
-}
+void sepia(int height, int width, RGBTRIPLE image[height][width]) { return; }
 
 // Reflect image horizontally
-void reflect(int height, int width, RGBTRIPLE image[height][width])
-{
-    return;
-}
+void reflect(int height, int width, RGBTRIPLE image[height][width]) { return; }
 
 // Blur image
-void blur(int height, int width, RGBTRIPLE image[height][width])
-{
-    return;
-}
+void blur(int height, int width, RGBTRIPLE image[height][width]) { return; }
