98413c54613618d94c8b81daf1f79b47bdda72ed
Daniel <Daniel.Burbridge.Developer@gmail.com>
2025-12-21 12:23:59 +0800

    this definitely is not correct

--- week-4/problem-set/volume/volume.c
+++ week-4/problem-set/volume/volume.c
@@ -6,38 +6,47 @@
 
 // Number of bytes in .wav header
 const int HEADER_SIZE = 44;
+typedef uint8_t BYTE;
 
-int main(int argc, char *argv[])
-{
-    // Check command-line arguments
-    if (argc != 4)
-    {
-        printf("Usage: ./volume input.wav output.wav factor\n");
-        return 1;
+int main(int argc, char *argv[]) {
+  // Check command-line arguments
+  if (argc != 4) {
+    printf("Usage: ./volume input.wav output.wav factor\n");
+    return 1;
+  }
+
+  // Open files and determine scaling factor
+  FILE *input = fopen(argv[1], "r");
+  if (input == NULL) {
+    printf("Could not open file.\n");
+    return 1;
+  }
+
+  FILE *output = fopen(argv[2], "w");
+  if (output == NULL) {
+    printf("Could not open file.\n");
+    return 1;
+  }
+
+  float factor = atof(argv[3]);
+
+  // TODO: Read samples from input file and write updated data to output
+  // file
+
+  BYTE b;
+
+  int count = 0;
+  while (fread(&b, sizeof(b), 1, input) != 0) {
+    count++;
+    if (count < HEADER_SIZE) {
+      fwrite(&b, sizeof(b), 1, output);
+    } else {
+      float c = b * factor;
+      fwrite(&c, sizeof(b), 1, output);
     }
+  }
 
-    // Open files and determine scaling factor
-    FILE *input = fopen(argv[1], "r");
-    if (input == NULL)
-    {
-        printf("Could not open file.\n");
-        return 1;
-    }
-
-    FILE *output = fopen(argv[2], "w");
-    if (output == NULL)
-    {
-        printf("Could not open file.\n");
-        return 1;
-    }
-
-    float factor = atof(argv[3]);
-
-    // TODO: Copy header from input file to output file
-
-    // TODO: Read samples from input file and write updated data to output file
-
-    // Close files
-    fclose(input);
-    fclose(output);
+  // Close files
+  fclose(input);
+  fclose(output);
 }
