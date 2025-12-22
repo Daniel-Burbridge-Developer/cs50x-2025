94785a45db98ae7456719b2c25930dc6d12d3a1a
Daniel <Daniel.Burbridge.Developer@gmail.com>
2025-12-22 21:27:50 +0800

    download filter-more distribution code

---
+++ week-4/problem-set/filter-more/helpers.c
@@ -0,0 +1,26 @@
+#include "helpers.h"
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
+  // Do the Grayscale thing
+  return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width]) {
+  // Do the reflect thing
+  return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width]) {
+  // Do the blur thing
+  return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width]) {
+  // Do the edges
+  //  thing
+  return;
+}
