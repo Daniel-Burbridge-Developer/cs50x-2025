cf74c876670da20c821e32487c1fa0443348d3e1
Daniel <Daniel.Burbridge.Developer@gmail.com>
2025-12-25 12:35:05 +0800

    FEAT: locate JPG files

--- week-4/problem-set/recover/recover.c
+++ week-4/problem-set/recover/recover.c
@@ -1,7 +1,36 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
-int main(int argc, char *argv[])
-{
- 
+
+typedef uint8_t BYTE;
+
+int main(int argc, char *argv[]) {
+  const int BLOCK_SIZE = 512;
+
+  if (argc != 2) {
+    printf("Usage: ./filter infile\n");
+    return 1;
+  }
+
+  char *infile = argv[1];
+
+  FILE *inptr = fopen(infile, "r");
+
+  if (inptr == NULL) {
+    printf("Could not open %s.\n", infile);
+    return 1;
+  }
+
+  BYTE block[512];
+
+  while (fread(block, 1, BLOCK_SIZE, inptr) != 0) {
+    if (block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff &&
+        (block[3] & 0xf0) == 0xe0) {
+      printf("Found a JPG\n");
+    } else {
+      printf("Did not fnd a JPG\n");
+    }
+  }
+
+  return 0;
 }
