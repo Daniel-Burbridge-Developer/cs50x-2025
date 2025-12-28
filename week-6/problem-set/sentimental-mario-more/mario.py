5d6cabc432d78e34f5434adf1277e1bf164ae04d
Daniel <Daniel.Burbridge.Developer@gmail.com>
2025-12-28 22:10:13 +0800

    double sided pyramid

--- week-6/problem-set/sentimental-mario-more/mario.py
+++ week-6/problem-set/sentimental-mario-more/mario.py
@@ -1,7 +1,15 @@
 from cs50 import get_int
 
-def main():
 
+def main():
+    num_height = 0
+    height = ""
+    while (num_height > 8) or (num_height <= 0):
+        height = get_int("Height: ")
+        num_height = int(height)
+
+    for i in range(num_height):
+        print((" " * (int(height) - (i + 1)) + "#" * (i + 1)) + ("  ") + "#" * (i + 1))
 
 
 if __name__ == "__main__":
