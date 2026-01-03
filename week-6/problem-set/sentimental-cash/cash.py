32a9d986a24c258daa8b1fcb446fc118e0b40583
Daniel <Daniel.Burbridge.Developer@gmail.com>
2026-01-03 20:54:25 +0800

    wip: handle correct change with floating point error

--- week-6/problem-set/sentimental-cash/cash.py
+++ week-6/problem-set/sentimental-cash/cash.py
@@ -2,7 +2,20 @@ from cs50 import get_float
 
 
 def main():
-    get_float()
+    change = get_float("Change: ")
+    coin_types = [0.25, 0.10, 0.05, 0.01]
+
+    while change < 0:
+        change = get_float("Change: ")
+
+    coin_count = 0
+    for coin in coin_types:
+        print(f"floating point error {change}")
+        while change >= coin:
+            coin_count += 1
+            change -= coin
+
+    print(f"{coin_count}")
 
 
 if __name__ == "__main__":
