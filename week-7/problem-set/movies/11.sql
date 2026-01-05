597313991a489a151351ad6358da294ec164b849
Daniel <Daniel.Burbridge.Developer@gmail.com>
2026-01-05 18:46:44 +0800

    wip: sql 8-11

--- week-7/problem-set/movies/11.sql
+++ week-7/problem-set/movies/11.sql
@@ -1,1 +1,21 @@
 -- 11. Titles of the five highest rated movies (in order) that Chadwick Boseman starred in, starting with the highest rated
+-- This one is certainly wrong currently.
+SELECT title
+FROM movies
+WHERE id IN (
+        SELECT movie_id
+        FROM ratings
+        WHERE movie_id IN (
+                (
+                    SELECT movie_id
+                    FROM stars
+                    WHERE person_id IN (
+                            SELECT id
+                            FROM people
+                            WHERE name = 'Chadwick Boseman'
+                        )
+                )
+            )
+        ORDER BY rating DESC
+        LIMIT 5
+    )
