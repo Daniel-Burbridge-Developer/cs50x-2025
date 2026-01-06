9aff97688669310297c8471531e3da64aa6baf15
Daniel <Daniel.Burbridge.Developer@gmail.com>
2026-01-06 21:33:26 +0800

    feat: 12-13.sql

--- week-7/problem-set/movies/12.sql
+++ week-7/problem-set/movies/12.sql
@@ -1,1 +1,7 @@
 -- 12. Titles of all of movies in which both Jennifer Lawrence and Bradley Cooper starred
+SELECT title
+FROM people
+    JOIN stars ON people.id = stars.person_id
+    JOIN movies ON stars.movie_id = movies.id
+WHERE name = 'Jennifer Lawrence'
+    OR name = 'Bradley Cooper'
--- week-7/problem-set/movies/13.sql
+++ week-7/problem-set/movies/13.sql
@@ -1,1 +1,12 @@
 -- 13. Names of all people who starred in a movie in which Kevin Bacon also starred
+SELECT people.name
+FROM people
+    JOIN stars ON people.id = stars.person_id
+    JOIN movies ON stars.movie_id = movies.id
+WHERE stars.movie_id IN (
+        SELECT id
+        FROM movies
+            JOIN stars ON people.id = stars.person_id
+            JOIN movies ON stars.movie_id = movies.id
+        WHERE people.name = 'Kevin Bacon'
+    )
