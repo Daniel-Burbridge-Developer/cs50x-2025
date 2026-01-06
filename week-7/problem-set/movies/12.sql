7b5914ac258afcb6577c09b4fc5c6bbdf26fa496
Daniel <Daniel.Burbridge.Developer@gmail.com>
2026-01-06 22:21:34 +0800

    feat: 12.sql

--- week-7/problem-set/movies/12.sql
+++ week-7/problem-set/movies/12.sql
@@ -1,7 +1,17 @@
 -- 12. Titles of all of movies in which both Jennifer Lawrence and Bradley Cooper starred
 SELECT title
-FROM people
-    JOIN stars ON people.id = stars.person_id
-    JOIN movies ON stars.movie_id = movies.id
-WHERE name = 'Jennifer Lawrence'
-    OR name = 'Bradley Cooper'
+FROM movies
+where movies.id IN (
+        SELECT movies.id
+        FROM movies
+            JOIN stars ON movies.id = stars.movie_id
+            JOIN people ON stars.person_id = people.id
+        WHERE name = 'Jennifer Lawrence'
+    )
+    AND movies.id IN (
+        SELECT movies.id
+        FROM movies
+            JOIN stars ON movies.id = stars.movie_id
+            JOIN people ON stars.person_id = people.id
+        WHERE name = 'Bradley Cooper'
+    )
