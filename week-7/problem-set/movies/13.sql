fce1190808656e85debcedba403951e4b2776d49
Daniel <Daniel.Burbridge.Developer@gmail.com>
2026-01-07 13:06:10 +0800

    feat: week 7 movies

--- week-7/problem-set/movies/13.sql
+++ week-7/problem-set/movies/13.sql
@@ -1,13 +1,20 @@
 -- 13. Names of all people who starred in a movie in which Kevin Bacon also starred
-SELECT people.name
+SELECT name
 FROM people
-    JOIN stars ON people.id = stars.person_id
-    JOIN movies ON stars.movie_id = movies.id
-WHERE stars.movie_id IN (
-        SELECT movies.id
-        FROM people
-            JOIN stars ON people.id = stars.person_id
-            JOIN movies ON stars.movie_id = movies.id
-        WHERE people.name = 'Kevin Bacon'
-            AND people.birth = 1958
+WHERE id IN (
+        SELECT person_id
+        FROM stars
+        WHERE movie_id IN (
+                (
+                    SELECT movie_id
+                    FROM stars
+                    WHERE person_id IN (
+                            SELECT id
+                            FROM people
+                            WHERE name = 'Kevin Bacon'
+                                AND birth = 1958
+                        )
+                )
+            )
     )
+    AND name != 'Kevin Bacon'
