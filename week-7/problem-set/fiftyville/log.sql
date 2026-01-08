ffb5a66c453849476c703e531a12872aeb1ba936
Daniel <Daniel.Burbridge.Developer@gmail.com>
2026-01-08 12:58:04 +0800

    wip: culprit found (lunch break successful)

--- week-7/problem-set/fiftyville/log.sql
+++ week-7/problem-set/fiftyville/log.sql
@@ -123,10 +123,42 @@ WHERE account_number IN (
     -- 686048
     -- 514354
     -- 449774
-    -- Let's find out a little about the flight data.
+    -- Let's find out a little about the flight data. First flight
 SELECT *
 FROM flights
-WHERE origin_airport_id IN (
-        SELECT id
-        FROM airports
+    JOIN airports ON flights.origin_airport_id = airports.id
+    JOIN passengers ON flights.id = passengers.flight_id
+    JOIN people ON passengers.passport_number = people.passport_number
+WHERE airports.city = 'Fiftyville'
+    AND year = 2025
+    AND month = 7
+    AND day = 29
+    AND people.id IN (
+        SELECT person_id
+        FROM bank_accounts
+        WHERE account_number IN (
+                SELECT account_number
+                FROM atm_transactions
+                WHERE year = 2025
+                    AND month = 7
+                    AND day = 28
+                    AND atm_location = 'Leggett Street' -- okay, so "transaction type" is listed as withdraw or deposit
+                    AND transaction_type = 'withdraw'
+            )
+            AND person_id IN (
+                SELECT people.id
+                FROM people
+                    JOIN bakery_security_logs on people.license_plate = bakery_security_logs.license_plate
+                    JOIN phone_calls on phone_calls.caller = people.phone_number
+                WHERE bakery_security_logs.year = 2025
+                    AND bakery_security_logs.month = 7
+                    AND bakery_security_logs.day = 28
+                    AND hour = 10
+                    AND minute >= 15
+                    AND minute <= 35
+                    AND duration < 60
+            )
     )
+ORDER BY minute ASC
+LIMIT 1 -- 18|8|6|2025|7|29|16|0|8|CSF|Fiftyville Regional Airport|Fiftyville|18|3592750733|4C|514354|Diana|(770) 555-1861|3592750733|322W7JE
+    -- Found you Diana!
