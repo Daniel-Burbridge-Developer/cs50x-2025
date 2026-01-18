1e4ce706957d7b636c5205b093a71ab97db58f72
Daniel <Daniel.Burbridge.Developer@gmail.com>
2026-01-18 20:03:54 +0800

    prototype

--- week-10/problem-set/grogu-image-generator/main.py
+++ week-10/problem-set/grogu-image-generator/main.py
@@ -1,21 +1,22 @@
 import os
 import random
 import time
-
 import requests
+from dotenv import load_dotenv
+from google import genai
 
 
-def generate_grogu():
-    # 1. The Prompt
-    # We include "flux" in the URL to force the smart model
-    prompt = "Cinematic shot of Baby Yoda (Grogu) wearing a knitted scarf, holding a cup of hot cocoa, autumn forest background, hyper-realistic, 8k, soft lighting, depth of field"
+def main():
+    load_dotenv()
+    prompt, file_name = generate_prompt()
+    generate_image(prompt, file_name)
 
-    # 2. The Random Seed
-    # This ensures a new image every time.
+
+def generate_image(
+    prompt="Cinematic shot of Baby Yoda (Grogu) wearing a knitted scarf, holding a cup of hot cocoa, autumn forest background, hyper-realistic, 8k, soft lighting, depth of field",
+    file_name="todays_image",
+):
     seed = random.randint(1, 1000000)
-
-    # 3. The Magic URL
-    # We pass 'model=flux' to get the high-quality AI
     url = f"https://image.pollinations.ai/prompt/{prompt}?width=1024&height=1024&seed={seed}&model=flux"
 
     print(f"Requesting Grogu from Pollinations (Seed: {seed})...")
@@ -24,12 +25,11 @@ def generate_grogu():
         response = requests.get(url, timeout=30)
 
         if response.status_code == 200:
-            # Save it to the current folder
-            filename = "grogu_today.jpg"
-            with open(filename, "wb") as f:
+            # Save it to the current folders
+            with open(file_name, "wb") as f:
                 f.write(response.content)
-            print(f"Success! Saved to {filename}")
-            return filename
+            print(f"Success! Saved to {file_name}")
+            return file_name
         else:
             print(f"Failed. Status Code: {response.status_code}")
             return None
@@ -39,5 +39,29 @@ def generate_grogu():
         return None
 
 
+def generate_prompt():
+    client = genai.Client()
+
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents="Generate a prompt to create a unique grogu image based on the most notable headline from today.",
+    )
+
+    image_generate_prompt = response.text
+    print(f"Image Prompt: {image_generate_prompt}")
+
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents=(
+            f"Please create a short file name based on the image this will create: {image_generate_prompt}"
+        ),
+    )
+
+    file_name = response.text
+    print(f"FIle Name: {file_name}")
+
+    return image_generate_prompt, file_name
+
+
 if __name__ == "__main__":
-    generate_grogu()
+    main()
